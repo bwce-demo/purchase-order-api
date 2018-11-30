3d34c4ac85229cd2757ff3834409adcce854350e
Rishi <rishi@demo.com>
2018-11-30 21:44:06 +0530

    API-fy PO Service

---
+++ SupplierPO_API.module/Tests/A3DEWS2RF4.ml
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="ASCII"?>
+<emulation:BWTFile xmi:version="2.0" xmlns:xmi="http://www.omg.org/XMI" xmlns:emulation="http:///emulation.ecore">
+  <CurrentLoadedTest href="platform:/resource/SupplierPO_API.module/Tests/RetailerDetails.bwt#/"/>
+  <EmulationDataFile>ACED000574001352657461696C657244657461696C732E627774</EmulationDataFile>
+</emulation:BWTFile>
